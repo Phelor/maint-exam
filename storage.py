584b01fe10f84ab98dcd207fac98415f864ae452
Baptiste R <bapt.rousselot@gmail.com>
2025-06-24 00:12:11 +0200

    pyLint

--- storage.py
+++ storage.py
@@ -3,12 +3,12 @@ import json
 import os
 from models import Task
 
-class StorageBackend(Protocol): 
+class StorageBackend(Protocol):
     """protocole = interface"""
     def load_tasks(self) -> List[Task]:
-        ...
+        """interface de load task (SOLID)"""
     def save_tasks(self, tasks: List[Task]) -> None:
-        ...
+        """interface de save task (SOLID)"""
 
 class JsonStorageBackend:
     """si le filename existe il le load ou sinon le crée"""
@@ -16,9 +16,10 @@ class JsonStorageBackend:
         self.filename = filename
 
     def load_tasks(self) -> List[Task]:
+        """Json load storage"""
         if not os.path.exists(self.filename):
             return []
-        with open(self.filename, "r") as f:
+        with open(self.filename, "r", encoding="utf-8") as f:
             try:
                 data = json.load(f)
                 return [Task.from_dict(t) for t in data]
@@ -26,5 +27,6 @@ class JsonStorageBackend:
                 return []
 
     def save_tasks(self, tasks: List[Task]) -> None:
-        with open(self.filename, "w") as f:
+        """Json save storage"""
+        with open(self.filename, "w", encoding="utf-8") as f:
             json.dump([t.to_dict() for t in tasks], f, indent=2)
