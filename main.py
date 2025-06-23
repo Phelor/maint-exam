3966383139b28ba9d6208605431ae47a3fabf45b
Baptiste R <bapt.rousselot@gmail.com>
2025-06-23 23:42:10 +0200

    implémentation de la lib argparse
    peut etre utilisr click dans le futur (mais du coup besoin d'un venv)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from manager import TaskManager 
 from storage import JsonStorageBackend
 
@@ -6,11 +7,29 @@ def main():
     """Main entry point for the task CLI application."""
     storage = JsonStorageBackend("tasks.json")
     manager = TaskManager(storage)
-    if len(sys.argv) < 2:
-        print("Usage: python sample_app.py [add|remove|list|find] [args]")
-        return
 
-    action = sys.argv[1]
+    parser = argparse.ArgumentParser(description="Task manager de fou là")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    parser_add = subparsers.add_parser("add", help="Add new task")
+    parser_add.add_argument("description", type=str, help="Task description")
+
+    parser_remove = subparsers.add_parser("remove", help="Remove a task by ID")
+    parser_remove.add_argument("id", type=int, help="Task ID")
+
+    parser_list = subparsers.add_parser("list", help="List all tasks")
+
+    parser_find = subparsers.add_parser("find", help="Find tasks by id")
+    parser_find.add_argument("keyword", type=str, help="id to search")
+
+    args = parser.parse_args()
+
+    # if len(sys.argv) < 2:
+    #     print("Usage: python sample_app.py [add|remove|list|find] [args]")
+    #     return
+
+    action = args.command
+
     if action == "add":
         if len(sys.argv) < 3:
             print("Please provide a task description.")
